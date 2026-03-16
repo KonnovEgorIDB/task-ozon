5021850631be0bf906726dbab07f0593e77beb95
parensdops <konnov.egor2014@yandex.ru>
2026-03-16 23:46:47 +0300

    feat: add template-based rendering to Progress constructor

--- src/main.js
+++ src/main.js
@@ -1,43 +1,38 @@
-import "./style.css";
-import Progress from "./api.js";
+import './style.css'
+import Progress from './api.js'
 
-const progress = new Progress(".progress__view", { value: 75 });
+const progress = new Progress('#progress-1')
+progress.setValue(75)
 
-const input = document.querySelector("#input-value");
-const switchHide = document.querySelector("#switch-hide");
-const switchAnimate = document.querySelector("#switch-animate");
+const input = document.querySelector('#input-value')
+const switchHide = document.querySelector('#switch-hide')
+const switchAnimate = document.querySelector('#switch-animate')
 
-input.addEventListener("input", (e) => {
-  const value = Number(e.target.value);
-  if (!isNaN(value) && value >= 0 && value <= 100) {
-    progress.setValue(value);
+input.addEventListener('input', event => {
+  const value = Number(event.target.value)
+  if (!Number.isNaN(value) && value >= 0 && value <= 100) {
+    progress.setValue(value)
   } else {
-    e.target.value = progress.getValue();
+    event.target.value = progress.getValue()
   }
-});
+})
 
-switchHide.addEventListener("change", (e) => {
-  if (e.target.checked) {
-    progress.setHidden(true);
+switchHide.addEventListener('change', event => {
+  if (event.target.checked) {
+    progress.setHidden(true)
   } else {
-    progress.setHidden(false);
+    progress.setHidden(false)
   }
-});
+})
 
-
-
-switchAnimate.addEventListener("change", (e) => {
-  if (e.target.checked) {
-
-    input.setAttribute("disabled", true);
-    input.value = null;
-
-    progress.setAnimated(true);
-
+switchAnimate.addEventListener('change', event => {
+  if (event.target.checked) {
+    input.setAttribute('disabled', true)
+    input.value = undefined
+    progress.setAnimated(true)
   } else {
-    progress.setAnimated(false);
-    input.removeAttribute("disabled");
-    input.value = progress.getValue();
-
+    progress.setAnimated(false)
+    input.removeAttribute('disabled')
+    input.value = progress.getValue()
   }
-});
+})
